4e91c91d49a2773d1d5da3c0d07b8dc7049a8c34
Mirza Ismayeel <ismayeelmirza@gmail.com>
2026-08-23 12:23:10 +0430

    Navbar done Sidebar remains

---
+++ JavaScript/Index.js
@@ -0,0 +1,8 @@
+let Sidebarclose = document.querySelector(".sidebar-close")
+let Navmenu = document.querySelector(".nav-menu")
+let Sidebar = document.querySelector(".sidebar-container")
+let Sidebarauto = ()=>{
+    Sidebar.classList.toggle('sidebar-container-open')
+}
+Navmenu.addEventListener("click", Sidebarauto)
+Sidebar.addEventListener("click", Sidebarauto)
