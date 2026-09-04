2995e12b5a787a7a3bbee050845652342ddd091e
Mirza Ismayeel <ismayeelmirza@gmail.com>
2026-09-04 19:53:29 +0430

    Improving some home and top dishies and menu section UI and UI

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -3,6 +3,7 @@ let Navmenu = document.querySelector(".nav-menu")
 let Sidebar = document.querySelector(".sidebar-container")
 let Sidebarauto = ()=>{
     Sidebar.classList.toggle('sidebar-container-open')
+    document.body.classList.toggle('overflow-hidden')
 }
 Navmenu.addEventListener("click", Sidebarauto)
 Sidebar.addEventListener("click", Sidebarauto)
